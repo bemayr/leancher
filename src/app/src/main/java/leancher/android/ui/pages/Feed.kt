30a3f4aebd6d3f64aa85641177eeb6165463a26e
bemayr <bernhard@mayr.io>
2021-01-13 10:45:36 +0100

    Fixed widget display 🎉

--- src/app/src/main/java/leancher/android/ui/pages/Feed.kt
+++ src/app/src/main/java/leancher/android/ui/pages/Feed.kt
@@ -1,13 +1,15 @@
 package leancher.android.ui.pages
 
+import android.appwidget.AppWidgetHost
 import android.appwidget.AppWidgetHostView
+import android.appwidget.AppWidgetManager
+import android.graphics.Color
+import android.util.Log
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.LinearLayout
 import android.widget.ListPopupWindow.MATCH_PARENT
 import android.widget.TextView
-import androidx.annotation.LayoutRes
-import androidx.compose.Compose
-import androidx.compose.View
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,9 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.viewinterop.AndroidViewHolder
-import androidx.compose.ui.viewinterop.InternalInteropApi
-import androidx.ui.core.ComposeView
 import leancher.android.R
 import leancher.android.ui.core.FeedState
 import leancher.android.ui.theme.White
@@ -35,42 +34,34 @@ fun Feed(page: Int, feedState: FeedState) {
 @Composable
 fun WidgetHostView(feedState: FeedState, state: MutableState<Int>) {
     if (feedState.widgets.size > 0 && feedState.hostViews.size > 0) {
-        AndroidView(viewBlock = { ctx ->
-            //Here you can construct your View
-            android.widget.Button(ctx).apply {
-                text = "MyAndroidButton"
-                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                setOnClickListener {
-                    state.value++
+        Column() {
+            AndroidView(viewBlock = { ctx ->
+                //Here you can construct your View
+                android.widget.Button(ctx).apply {
+                    text = "Some Button"
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+                    setOnClickListener {
+                        state.value++
+                    }
                 }
-            }
-        }, modifier = Modifier.padding(8.dp))
+            }, modifier = Modifier.padding(8.dp))
 
-        AndroidView(viewBlock = { ctx ->
-            //Here you can construct your View
-            TextView(ctx).apply {
-                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                text = "abc"
-            }
-        }, update = {
-            it.text = "You have clicked the buttons: " + state.value.toString() + " times"
-        })
+            AndroidView(viewBlock = { ctx ->
+                //Here you can construct your View
+                TextView(ctx).apply {
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+                    text = "abc"
+                    setTextColor(Color.WHITE)
+                }
+            }, update = {
+                it.text = "You have clicked the buttons: " + state.value.toString() + " times"
+            })
 
-        AndroidView(viewBlock = { ctx ->
-            LinearLayout(ctx).apply {
-                addView(androidx.compose.ui.platform.ComposeView(ctx).apply {
-                    id = R.id.main_layout
-                })
-            }
-        })
-
-        AndroidView(viewBlock = { ctx ->
-            println("id: " + feedState.widgets[0].id)
-            println("info: " + feedState.widgets[0].providerInfo)
-            AppWidgetHostView(ctx).apply {
-                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                setAppWidget(feedState.widgets[0].id, feedState.widgets[0].providerInfo)
-            }
-        })
+            AndroidView(viewBlock = { ctx ->
+                feedState.hostViews.first().apply {
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+                }
+            })
+        }
     }
 }
