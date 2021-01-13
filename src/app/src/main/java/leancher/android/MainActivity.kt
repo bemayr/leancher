a2feeb38fe45644949715dcd16f734c1bb704d51
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2021-01-13 11:29:08 +0100

    Added a fancy paginator

--- src/app/src/main/java/leancher/android/MainActivity.kt
+++ src/app/src/main/java/leancher/android/MainActivity.kt
@@ -4,11 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.AmbientAnimationClock
 import androidx.compose.ui.platform.setContent
@@ -16,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import leancher.android.ui.components.Pager
 import leancher.android.ui.components.PagerState
+import leancher.android.ui.components.Paginator
 import leancher.android.ui.pages.Feed
 import leancher.android.ui.pages.Home
 import leancher.android.ui.pages.NotificationCenter
@@ -36,24 +36,31 @@ class MainActivity : AppCompatActivity() {
     fun PagerTest() {
         val clock = AmbientAnimationClock.current
         val pagerState = remember(clock) { PagerState(clock, 1, 0, 2) }
+        val currentPage = pagerState.currentPage
 
         run {
             val clock = AmbientAnimationClock.current
             remember(clock) { PagerState(clock, 1, 0, 2) }
         }
 
-        val modifier = Modifier.fillMaxSize()
-
         Pager(state = pagerState) {
-            Column(
-                    modifier.padding(horizontal = 12.dp, vertical = 8.dp)
-            ) {
-                when(page) {
-                    0 -> Feed(page)
-                    1 -> Home(page, launchIntent = { launchIntent() })
-                    2 -> NotificationCenter(page)
-                    else -> Home(page, launchIntent = { launchIntent() })
+            Row(Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 12.dp, vertical = 8.dp)) {
+                Column() {
+                    when(page) {
+                        0 -> Feed(page)
+                        1 -> Home(page, launchIntent = { launchIntent() })
+                        2 -> NotificationCenter(page)
+                        else -> Home(page, launchIntent = { launchIntent() })
+                    }
                 }
+            }
+        }
+        Row(Modifier
+                .fillMaxSize(), verticalAlignment = Alignment.Bottom) {
+            Column() {
+                Paginator(pageAmount = 3, currentPage = currentPage)
             }
         }
     }
